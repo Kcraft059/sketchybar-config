ac7602f487d768ebc6c3b7a2c0cbdd37c1c5a4f2
Kcraft059 <kcraft059.msg@gmail.com>
2025-10-29 14:09:17 +0100

    removed unecessary log senders

--- plugins/music/script-separator.sh
+++ plugins/music/script-separator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH=/opt/homebrew/bin/:$PATH
 export RELPATH=$(dirname $0)/../..
-source $RELPATH/log_handler.sh
+#source $RELPATH/log_handler.sh
 
 GRAPHSTATE="$(sketchybar --query graph | sed 's/\\n//g; s/\\\$//g; s/\\ //g' | jq -r '.geometry.drawing')"
 MUSICSTATE="$(sketchybar --query music | sed 's/\\n//g; s/\\\$//g; s/\\ //g' | jq -r '.geometry.drawing')"
@@ -14,7 +14,7 @@ if [ "$MUSICSTATE" = "on" ]; then ((++activitycount)); fi
 ### Only show middle separator if an activity is present
 
 if [ $activitycount -gt 0 ]; then
-	sendLog "Show activity separator (activity count=$activitycount)" "vomit"
+	#sendLog "Show activity separator (activity count=$activitycount)" "vomit"
   sketchybar --set separator_center drawing=on
 else
   sketchybar --set separator_center drawing=off
