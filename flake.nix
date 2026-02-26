f8ca32d1881ea91c69aae029e76e5e76c9a88d26
Kcraft059 <kcraft059.msg@gmail.com>
2026-02-26 15:46:20 +0100

    sbarlua overlay

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    sbarlua = {
+      url = "github:FelixKratz/SbarLua";
+      flake = false;
+    };
   };
 
   outputs =
@@ -8,18 +12,57 @@
     let
       system = "aarch64-darwin";
 
+      sbarlua-derivation =
+        { pkgs, inputs, ... }:
+        pkgs.stdenv.mkDerivation {
+          name = "sbarlua";
+
+          src = inputs.sbarlua;
+
+          nativeBuildInputs = [
+            pkgs.gcc
+          ];
+
+          buildInputs = [
+            pkgs.readline
+          ];
+
+          installPhase = ''
+            mkdir -p $out/lib/sketchybar
+            cp bin/sketchybar.so $out/lib/sketchybar/
+          '';
+        };
+
       pkgs = import nixpkgs {
         inherit system;
+        overlays = [
+          (self: super: {
+            sbarlua = super.callPackage sbarlua-derivation { inherit inputs; };
+          })
+        ];
       };
+
     in
     {
+      packages."${system}".sbarlua = pkgs.sbarlua;
+
       devShells."${system}".default = pkgs.mkShell {
         packages = with pkgs; [
           sketchybar
+          sbarlua
+          lua
         ];
 
         shellHook = ''
-          exec ${pkgs.zsh}/bin/zsh
+          export SKETCHYBAR_LUA_PATH="${pkgs.sbarlua}/lib/sketchybar"
+
+          cat > .zsh-shell <<'EOF'
+          TRAPEXIT() {
+            rm -rf .zsh-shell
+          }
+          EOF
+
+          exec ${pkgs.zsh}/bin/zsh --rcs -i -c "source .zsh-shell; ${pkgs.zsh}/bin/zsh -i"
         '';
       };
     };
