8260b1db8b123a7bad55e687a283df62817c9f7e
Kcraft059 <kcraft059.msg@gmail.com>
2026-03-16 07:47:14 +0100

    fix freezed time

--- items/date.lua
+++ items/date.lua
@@ -1,45 +1,57 @@
-local mod = { }
+local mod = {}
 
 -- Setup
 function mod.setup(palette)
   mod.properties = {
-    position    = "right",
+    position = "right",
     update_freq = 60,
 
     icon = {
-      --string        = "+%a %d. %b",
-      color         = palette.text.primary,
-      font          = { style = "Black", size = 12.0 },
-      padding_right = 5,
+      -- string        = "+%a %d. %b",
+      color = palette.text.primary,
+      font = {
+        style = "Black",
+        size = 12.0
+      },
+      padding_right = 5
     },
-    
+
     label = {
-      --string = "+%H:%M",
-      font          = { style = "Semibold", size = 13.0 },
-      width  = 50,
-      align  = "center"
+      -- string = "+%H:%M",
+      font = {
+        style = "Semibold",
+        size = 13.0
+      },
+      width = 50,
+      align = "center"
     }
   }
   return mod
 end
 
 local function parseDate(item, string)
-  local date = strSplit(string,"|")
+  local date = strSplit(string, "|")
   item:set({
-    icon  = { string = date[1] },
-    label = { string = date[2] }
+    icon = {
+      string = date[1]
+    },
+    label = {
+      string = date[2]
+    }
   })
-end 
+end
 
 -- Load 
 function mod.load()
-  mod.item = sbar.add("item",mod.properties)
-  mod.item:subscribe({"routine","forced"}, function(env) 
-    if env.SENDER == "forced" then 
-      sbar.exec("printf \"$(date '+%a %d. %b')|$(date '+%H:%M')\"", function (result,exit_code)
-        parseDate(mod.item,result)
-      end) 
+  mod.item = sbar.add("item", mod.properties)
+  mod.item:subscribe({"routine", "forced", "system_woke"}, function(env)
+    if (env.SENDER == "forced" or env.SENDER == "system_woke") then
+      sbar.exec("printf \"$(date '+%a %d. %b')|$(date '+%H:%M')\"", function(result, exit_code)
+        parseDate(mod.item, result)
+      end)
     end
+
+    log("date",env.NAME)
 
     sbar.exec([=[
       sleep $((59 - $(date '+%-S')))
@@ -47,8 +59,8 @@ function mod.load()
 		    sleep 0.1
 	    done
       printf "$(date '+%a %d. %b')|$(date '+%H:%M')"
-    ]=],function (result,exit_code) 
-      parseDate(mod.item,result)
+    ]=], function(result, exit_code)
+      parseDate(mod.item, result)
     end)
   end)
 
